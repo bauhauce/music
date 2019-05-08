b3d7f90f09e0242707e127499a855652739f757c
bauhaus <uthink@163.com>
2019-05-08 16:20:39 +0800

    add recommend rank

--- src/components/Rank.vue
+++ src/components/Rank.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <ul class="wrapper">
-      <li class="item">
-        <div class="item-icon"></div>
+      <li class="item" v-for="list of topList" :key="list.id">
+        <div class="item-icon">
+          <img :src="list.picUrl" alt="图片">
+        </div>
         <ul class="item-songlist">
-          <li class="song"><span>1</span><span>safasdfasdfafs</span></li>
-          <li class="song"><span>2</span><span>safasdfasdfafs</span></li>
-          <li class="song"><span>3</span><span>safasdfasdfafs</span></li>
+          <li class="song" v-for="(song, index) of list.songList" :key="index"><span>{{index + 1}}</span><span>{{song.singername + '-' + song.songname}}</span></li>
         </ul>
       </li>
     </ul>
@@ -14,8 +14,30 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'Rank'
+  name: 'Rank',
+  data () {
+    return {
+      topList: []
+    }
+  },
+  mounted () {
+    this.getToplist()
+  },
+  methods: {
+    getToplist () {
+      axios.get('https://www.easy-mock.com/mock/5cd273467c940631de15db31/music/rank').then(this.getToplistSucc)
+    },
+    getToplistSucc (res) {
+      res = res.data
+      if (res.code === 0 && res.data) {
+        const data = res.data
+        this.topList = data.topList
+      }
+    }
+  }
 }
 </script>
 
@@ -25,17 +47,18 @@ export default {
     width: 100%
     .item
       display: flex
-      magin-bottom: .4rem
+      margin-bottom: .4rem
       padding: 0 .4rem
       .item-icon
         flex: 0 0 1.6rem
         width: 1.6rem
         height: 1.6rem
         background: #ddd
+        img
+          width: 100%
       .item-songlist
         flex: 1
         padding-left: .4rem
-        background: #eee
         display: flex
         flex-direction: column
         justify-content: center
@@ -45,7 +68,7 @@ export default {
           overflow: hidden
           white-space: nowrap
           text-overflow: ellipsis
-          color: #666
+          color: #999
           span
             padding: 0 .05rem
 </style>
--- src/components/Recommend.vue
+++ src/components/Recommend.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <div class="banner"></div>
+    <div class="banner">
+      <swiper :options="swiperOption">
+        <swiper-slide v-for="item of sliderList" :key="item.id">
+          <img :src="item.picUrl" alt="图片">
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
     <dl class="content">
       <dt class="title">热门歌单推荐</dt>
-      <dd class="item">
-        <div class="item-left"></div>
+      <dd class="item" v-for="item of songList" :key="item.dissid">
+        <div class="item-left">
+          <img :src="item.imgurl" alt="图片">
+        </div>
         <div class="item-right">
-          <h3 class="item-title">title</h3>
-          <p class="item-desc">desc</p>
+          <h3 class="item-title">{{item.creator.name}}</h3>
+          <p class="item-desc">{{item.dissname}}</p>
         </div>
       </dd>
     </dl>
@@ -15,8 +24,40 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'recommend'
+  name: 'recommend',
+  data () {
+    return {
+      swiperOption: {
+        autoplay: true,
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination'
+        }
+      },
+      sliderList: [],
+      songList: []
+    }
+  },
+  mounted () {
+    this.getSlider()
+  },
+  methods: {
+    getSlider () {
+      axios.get('https://www.easy-mock.com/mock/5cd273467c940631de15db31/music/recommend')
+        .then(this.getSliderSucc)
+    },
+    getSliderSucc (res) {
+      res = res.data
+      if (res.code === 0 && res.data) {
+        const data = res.data
+        this.sliderList = data.slider
+        this.songList = data.songList
+      }
+    }
+  }
 }
 </script>
 
@@ -27,12 +68,14 @@ export default {
     padding-bottom: 40%
     overflow: hidden
     background: #ccc
+    img
+      width: 100%
   .content
     .title
       padding: .3rem 0
       text-align: center
       font-size: .3rem
-      color: #333
+      color: #ccc
     .item
       padding-left: .2rem
       padding-bottom: .2rem
@@ -41,15 +84,18 @@ export default {
         width: 1.2rem
         height: 1.2rem
         background: #ccc
+        img
+          width: 100%
       .item-right
         flex: 1
         padding-left: .2rem
-        background: #ddd
+        // background: #ddd
         .item-title
           padding: .12rem 0
           font-size: .3rem
+          color: #eee
         .item-desc
           padding: .12rem 0
           font-size: .3rem
-          color: #ccc
+          color: #666
 </style>
